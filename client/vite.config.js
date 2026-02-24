1d62ab27b45383fbb9c5ebca7a77f3ec91716d99
Tarik <topictarik82@gmail.com>
2026-02-24 23:03:38 +0100

    fixing vite problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["plansync-frontend-app-j3mhf.ondigitalocean.app"],
+    port: 8080,
+    host: true,
+  },
+});
